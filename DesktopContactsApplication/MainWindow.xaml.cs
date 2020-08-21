b1f017657221831d0f303b24b192a5f502db46eb
Zaidov012 <zaidov012@gmail.com>
2020-08-21 18:18:09 +0400

    First commit

---
+++ DesktopContactsApplication/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Contact> contacts;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            contacts = new List<Contact>();
+
+            ReadDatabase();
+        }
+
+        private void NewContactButton_Click(object sender, RoutedEventArgs e)
+        {
+            NewContactWindow newContactWindow = new NewContactWindow();
+            newContactWindow.ShowDialog();
+            
+            ReadDatabase();
+        }
+
+        void ReadDatabase()
+        {
+            
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                contacts = connection.Table<Contact>().OrderBy(c => c.Name).ToList();
+            }
+
+            if (contacts != null)
+            {
+                //foreach(var item in contacts)
+                //{
+                //    contactsListView.Items.Add(new ListViewItem()
+                //    {
+                //        Content = item
+                //    });
+                //}
+                contactsListView.ItemsSource = contacts;
+            }
+
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox searchTextBox = sender as TextBox;
+            List<Contact> filteredList = contacts.Where(c => c.Name.ToLower().Contains(searchTextBox.Text.ToLower())).ToList();
+            contactsListView.ItemsSource = filteredList;
+        }
+    }
+}
